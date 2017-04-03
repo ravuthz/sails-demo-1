2781b846b696f61f6aa4dd95a2e1fa0db93dd3ef
Yo Vannaravuth <ravuthz@gmail.com>
2017-04-03 02:43:26 +0000

    Add Customer and Stock relationship

--- api/models/Stock.js
+++ api/models/Stock.js
@@ -1,5 +1,5 @@
 /**
- * Customer.js
+ * Stock.js
  *
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
@@ -8,20 +8,17 @@
 module.exports = {
 
   attributes: {
-    name: {
-      type: 'string',
-      required: true
+    symbol: {
+        type: 'string',
+        required: true
     },
-    email: {
-      type: 'string',
-      email: true
+    number_of_shares: {
+        type: 'string',
+        required: true
     },
-    state: {
-      type: 'string'
-    },
-    stocks: {
-        collection: 'stock',
-        via: 'owner'
+    owner: {
+        model: 'customer',
+        required: true
     }
   }
   
